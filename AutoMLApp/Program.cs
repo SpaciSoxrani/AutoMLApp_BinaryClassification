d8286d91c7db2e6f78abc4e9677d17c7274f067e
SpaciSoxrani <thorloky007@gmail.com>
2021-07-21 20:23:06 +0500

    Добавьте файлы проекта.

---
+++ AutoMLApp/Program.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
+using AutoMLApp.DataStructures;
+using System.Collections.Generic;
+
+namespace AutoMLApp
+{
+    class Program
+    {
+        private const int Width = 114;
+
+        private static readonly string BaseDatasetsRelativePath = @"../../../datasets";
+        private static readonly string TrainDataRelativePath = $"{BaseDatasetsRelativePath}/wikipedia-detox-250-line-data.tsv";
+        private static readonly string TestDataRelativePath = $"{BaseDatasetsRelativePath}/wikipedia-detox-250-line-test.tsv";
+        private static string TrainDataPath = GetAbsolutePath(TrainDataRelativePath);
+        private static string TestDataPath = GetAbsolutePath(TestDataRelativePath);
+
+        private static readonly string BaseModelsRelativePath = @"../../../MLModels";
+        private static readonly string ModelRelativePath = $"{BaseModelsRelativePath}/SentimentModel.zip";
+        private static string ModelPath = GetAbsolutePath(ModelRelativePath);
+
+        private static uint ExperimentTime = 60;
+
+        static void Main(string[] args)
+        {
+            var mlContext = new MLContext();
+
+            // Create, train, evaluate and save a model
+            BuildTrainEvaluateAndSaveModel(mlContext);
+
+            // Make a single test prediction loading the model from .ZIP file
+            TestSinglePrediction(mlContext);
+
+            Console.WriteLine("=============== End of process, hit any key to finish ===============");
+            Console.ReadKey();
+        }
+
+        private static ITransformer BuildTrainEvaluateAndSaveModel(MLContext mlContext)
+        {
+            // STEP 1: Load data
+            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<SentimentIssue>(TrainDataPath, hasHeader: true);
+            IDataView testDataView = mlContext.Data.LoadFromTextFile<SentimentIssue>(TestDataPath, hasHeader: true);
+
+            // STEP 2: Display first few rows of training data
+            ShowDataViewInConsole(mlContext, trainingDataView);
+
+            // STEP 3: Initialize our user-defined progress handler that AutoML will 
+            // invoke after each model it produces and evaluates.
+            var progressHandler = new BinaryExperimentProgressHandler();
+
+            // STEP 4: Run AutoML binary classification experiment
+            ConsoleWriteHeader("=============== Running AutoML experiment ===============");
+            Console.WriteLine($"Running AutoML binary classification experiment for {ExperimentTime} seconds...");
+            ExperimentResult<BinaryClassificationMetrics> experimentResult = mlContext.Auto()
+                .CreateBinaryClassificationExperiment(ExperimentTime)
+                .Execute(trainingDataView, progressHandler: progressHandler);
+
+            // Print top models found by AutoML
+            Console.WriteLine();
+            PrintTopModels(experimentResult);
+
+            // STEP 5: Evaluate the model and print metrics
+            ConsoleWriteHeader("=============== Evaluating model's accuracy with test data ===============");
+            RunDetail<BinaryClassificationMetrics> bestRun = experimentResult.BestRun;
+            ITransformer trainedModel = bestRun.Model;
+            var predictions = trainedModel.Transform(testDataView);
+            var metrics = mlContext.BinaryClassification.EvaluateNonCalibrated(data: predictions, scoreColumnName: "Score");
+            PrintBinaryClassificationMetrics(bestRun.TrainerName, metrics);
+
+            // STEP 6: Save/persist the trained model to a .ZIP file
+            mlContext.Model.Save(trainedModel, trainingDataView.Schema, ModelPath);
+
+            Console.WriteLine("The model is saved to {0}", ModelPath);
+
+            return trainedModel;
+        }
+
+        // (OPTIONAL) Try/test a single prediction by loading the model from the file, first.
+        private static void TestSinglePrediction(MLContext mlContext)
+        {
+            ConsoleWriteHeader("=============== Testing prediction engine ===============");
+            SentimentIssue sampleStatement = new SentimentIssue { Text = "This is a very rude movie" };
+
+            ITransformer trainedModel = mlContext.Model.Load(ModelPath, out var modelInputSchema);
+            Console.WriteLine($"=============== Loaded Model OK  ===============");
+
+            // Create prediction engine related to the loaded trained model
+            var predEngine = mlContext.Model.CreatePredictionEngine<SentimentIssue, SentimentPrediction>(trainedModel);
+            Console.WriteLine($"=============== Created Prediction Engine OK  ===============");
+            // Score
+            var predictedResult = predEngine.Predict(sampleStatement);
+
+            Console.WriteLine($"=============== Single Prediction  ===============");
+            Console.WriteLine($"Text: {sampleStatement.Text} | Prediction: {(Convert.ToBoolean(predictedResult.Prediction) ? "Toxic" : "Non Toxic")} sentiment");
+            Console.WriteLine($"==================================================");
+        }
+
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+            return fullPath;
+        }
+
+        /// <summary>
+        /// Prints top models from AutoML experiment.
+        /// </summary>
+        private static void PrintTopModels(ExperimentResult<BinaryClassificationMetrics> experimentResult)
+        {
+            // Get top few runs ranked by accuracy
+            var topRuns = experimentResult.RunDetails
+                .Where(r => r.ValidationMetrics != null && !double.IsNaN(r.ValidationMetrics.Accuracy))
+                .OrderByDescending(r => r.ValidationMetrics.Accuracy).Take(3);
+
+            Console.WriteLine("Top models ranked by accuracy --");
+        }
+
+        public static void ShowDataViewInConsole(MLContext mlContext, IDataView dataView, int numberOfRows = 4)
+        {
+            string msg = string.Format("Show data in DataView: Showing {0} rows with the columns", numberOfRows.ToString());
+            ConsoleWriteHeader(msg);
+
+            var preViewTransformedData = dataView.Preview(maxRows: numberOfRows);
+
+            foreach (var row in preViewTransformedData.RowView)
+            {
+                var ColumnCollection = row.Values;
+                string lineToPrint = "Row--> ";
+                foreach (KeyValuePair<string, object> column in ColumnCollection)
+                {
+                    lineToPrint += $"| {column.Key}:{column.Value}";
+                }
+                Console.WriteLine(lineToPrint + "\n");
+            }
+        }
+
+        public static void ConsoleWriteHeader(params string[] lines)
+        {
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" ");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            var maxLength = lines.Select(x => x.Length).Max();
+            Console.WriteLine(new string('#', maxLength));
+            Console.ForegroundColor = defaultColor;
+        }
+
+        public static void PrintBinaryClassificationMetrics(string name, BinaryClassificationMetrics metrics)
+        {
+            Console.WriteLine($"************************************************************");
+            Console.WriteLine($"*       Metrics for {name} binary classification model      ");
+            Console.WriteLine($"*-----------------------------------------------------------");
+            Console.WriteLine($"*       Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"*       Area Under Curve:      {metrics.AreaUnderRocCurve:P2}");
+            Console.WriteLine($"*       Area under Precision recall Curve:  {metrics.AreaUnderPrecisionRecallCurve:P2}");
+            Console.WriteLine($"*       F1Score:  {metrics.F1Score:P2}");
+            //Console.WriteLine($"*       LogLoss:  {metrics.LogLoss:#.##}");
+            //Console.WriteLine($"*       LogLossReduction:  {metrics.LogLossReduction:#.##}");
+            Console.WriteLine($"*       PositivePrecision:  {metrics.PositivePrecision:#.##}");
+            Console.WriteLine($"*       PositiveRecall:  {metrics.PositiveRecall:#.##}");
+            Console.WriteLine($"*       NegativePrecision:  {metrics.NegativePrecision:#.##}");
+            Console.WriteLine($"*       NegativeRecall:  {metrics.NegativeRecall:P2}");
+            Console.WriteLine($"************************************************************");
+        }
+
+        private static void CreateRow(string message, int width)
+        {
+            Console.WriteLine("|" + message.PadRight(width - 2) + "|");
+        }
+
+        internal static void PrintIterationMetrics(int iteration, string trainerName, BinaryClassificationMetrics metrics, double? runtimeInSeconds)
+        {
+            CreateRow($"{iteration,-4} {trainerName,-35} {metrics?.Accuracy ?? double.NaN,9:F4} {metrics?.AreaUnderRocCurve ?? double.NaN,8:F4} {metrics?.AreaUnderPrecisionRecallCurve ?? double.NaN,8:F4} {metrics?.F1Score ?? double.NaN,9:F4} {runtimeInSeconds.Value,9:F1}", Width);
+        }
+
+        internal static void PrintIterationMetrics(int iteration, string trainerName, MulticlassClassificationMetrics metrics, double? runtimeInSeconds)
+        {
+            CreateRow($"{iteration,-4} {trainerName,-35} {metrics?.MicroAccuracy ?? double.NaN,14:F4} {metrics?.MacroAccuracy ?? double.NaN,14:F4} {runtimeInSeconds.Value,9:F1}", Width);
+        }
+
+        internal static void PrintIterationMetrics(int iteration, string trainerName, RegressionMetrics metrics, double? runtimeInSeconds)
+        {
+            CreateRow($"{iteration,-4} {trainerName,-35} {metrics?.RSquared ?? double.NaN,8:F4} {metrics?.MeanAbsoluteError ?? double.NaN,13:F2} {metrics?.MeanSquaredError ?? double.NaN,12:F2} {metrics?.RootMeanSquaredError ?? double.NaN,8:F2} {runtimeInSeconds.Value,9:F1}", Width);
+        }
+
+        internal static void PrintIterationMetrics(int iteration, string trainerName, RankingMetrics metrics, double? runtimeInSeconds)
+        {
+            CreateRow($"{iteration,-4} {trainerName,-15} {metrics?.NormalizedDiscountedCumulativeGains[0] ?? double.NaN,9:F4} {metrics?.NormalizedDiscountedCumulativeGains[2] ?? double.NaN,9:F4} {metrics?.NormalizedDiscountedCumulativeGains[9] ?? double.NaN,9:F4} {metrics?.DiscountedCumulativeGains[9] ?? double.NaN,9:F4} {runtimeInSeconds.Value,9:F1}", Width);
+        }
+
+        internal static void PrintIterationException(Exception ex)
+        {
+            Console.WriteLine($"Exception during AutoML iteration: {ex}");
+        }
+
+
+        internal static void PrintBinaryClassificationMetricsHeader()
+        {
+            CreateRow($"{"",-4} {"Trainer",-35} {"Accuracy",9} {"AUC",8} {"AUPRC",8} {"F1-score",9} {"Duration",9}", Width);
+        }
+
+        public class BinaryExperimentProgressHandler : IProgress<RunDetail<BinaryClassificationMetrics>>
+        {
+            private int _iterationIndex;
+
+            public void Report(RunDetail<BinaryClassificationMetrics> iterationResult)
+            {
+                if (_iterationIndex++ == 0)
+                {
+                    PrintBinaryClassificationMetricsHeader();
+                }
+
+                if (iterationResult.Exception != null)
+                {
+                    PrintIterationException(iterationResult.Exception);
+                }
+                else
+                {
+                    PrintIterationMetrics(_iterationIndex, iterationResult.TrainerName,
+                        iterationResult.ValidationMetrics, iterationResult.RuntimeInSeconds);
+                }
+            }
+
+        }
+    }
+}
